32b17286cb1aa4968c423911cf3b2ee651b68b05
deuk <weg0430@naver.com>
2023-11-09 10:41:24 +0900

    controller update

--- src/main/java/com/example/demo/CustomerController.java
+++ src/main/java/com/example/demo/CustomerController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.accessingdatajpa.Customer;
 import com.example.demo.accessingdatajpa.CustomerRepository;
@@ -10,6 +11,13 @@ import com.example.demo.accessingdatajpa.CustomerRepository;
 @RestController // Controller, ResponseBody
 public class CustomerController {
 	@Autowired CustomerRepository repo;
+	
+	@GetMapping("/")
+	public ModelAndView main() {
+		ModelAndView mv = new ModelAndView("home");
+		mv.addObject("greet", "<b>안녕</b>하세요");
+		return mv;
+	}
 	
 	@GetMapping("/customer")
 	public Iterable<Customer> finall(){
